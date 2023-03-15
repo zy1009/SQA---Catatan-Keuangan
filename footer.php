bae01ef826b340ebfd3ad1055a9ef2f916cf2309
zy1009 <127292242+zy1009@users.noreply.github.com>
2023-03-15 08:26:27 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Kelompok 2 2019</span> | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+            
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
